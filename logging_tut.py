54ccbc5560c59089f4ff70d2156653bbce305f15
nabindra <nabindrashrestha10@gmail.com>
2024-04-18 11:53:51 +0545

    added a logging module

---
+++ logging_tut.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(format='%(process)d-%(asctime)s-%(levelname)s-%(message)s')
+logging.warning("This is a warning")
+logging.warning("This is another warning")
+
+
+
